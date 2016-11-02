fbeb914cc27b855ba5131b4a3eefe86f0d354b9d
Brin Ryder <brin.ryder@gmail.com>
2016-11-02 15:15:19 +1300

    DID SOME MORE STUFF, GOD DAMN

--- r-novice-inflammation.R
+++ r-novice-inflammation.R
@@ -1,7 +1,7 @@
 magic <- c("Monkey", "Dog", "Fish", "Zebra", "Cat", "Long horse", "Cantaloupe", "Tiger", "Beetle", "Leopard", "Snail", "Chihuahua", "Yes")
 loadPtData <- function(datadir,datapattern, printall, makepdf, domagic)
 {
-  if(makepdf) pdf(paste("Report ",Sys.Date(),".pdf"))
+  if(makepdf) pdf(paste0("Report ",Sys.Date(),".pdf"))
   i <- 0
   filenames = list.files(path=datadir,pattern=datapattern,full.names=TRUE)
   for(ptfile in filenames) 
@@ -12,12 +12,12 @@ loadPtData <- function(datadir,datapattern, printall, makepdf, domagic)
     if(domagic) ptid <- magic[ptid]
     print(paste("Looking at",ptfile))
     ptdata <- read.csv(paste(getwd(),ptfile, sep = "/"), header = FALSE)
-    if(makepdf) par(mfrow=c(2,3))
-    plot(apply(ptdata,2,mean), main=paste("Patient",ptid), ylab="Mean inflammations")
+    par(mfrow=c(2,3))
+    plot(apply(ptdata,2,mean), main=paste("Patient",ptid), ylab="Mean inflammations", pch="$$$$$")
     if(printall)
     {
-      plot(apply(ptdata,2,min), main=paste("Patient",ptid), ylab="Minimum inflammations")
-      plot(apply(ptdata,2,max), main=paste("Patient",ptid), ylab="Maximum inflammations")
+      plot(apply(ptdata,2,min), main=paste("Patient",ptid), ylab="Minimum inflammations", pch="@")
+      plot(apply(ptdata,2,max), main=paste("Patient",ptid), ylab="Maximum inflammations", pch="&")
     }
   }
   if(makepdf) dev.off()
